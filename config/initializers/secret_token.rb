3383fd2a71c13934385b99645a80db5dae4b17e4
Brian Zollinhofer <brian.zollinhofer@gmail.com>
2011-04-28 13:31:11 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinalTutApp::Application.config.secret_token = '3cba984b22104610567d02e4a3622e4a9c74bca84f9feb801f6b1c2e45db0be2498a14f92d72c189397266a0e3cd38fb838a52038a9bec00545d6dee436136b4'
